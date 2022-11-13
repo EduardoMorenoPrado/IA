949b4d3c8c3d210b0a924bb30972a414480d44c0
EduardoMorenoPrado <118079336+EduardoMorenoPrado@users.noreply.github.com>
2022-11-13 14:15:23 +0100

    Add files via upload
    Neuronal network that detects the formula to go from Celsius to Farenheit with 7 examples

---
+++ FirstIAProject/BrainLinks.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+
+
+celsius= np.array([-40,-10,0,8,15,22,38], dtype= float)
+fahremheit = np.array ([-40,14,32,46,59,72,100], dtype= float)
+
+layer= tf.keras.layers.Dense(units=1, input_shape=[1])
+model= tf.keras.Sequential ([layer])
+
+model.compile(
+    optimizer= tf.keras.optimizers.Adam(0.1),
+    loss= "mean_squared_error" 
+)
+
+print ("start")
+historial= model.fit(celsius,fahremheit, epochs=1000, verbose=False)
+print ("done")
+  # dense layers since all neurons can connect all neurons, the firs 
+import matplotlib.pyplot as plt
+plt.ylabel("loss magnitude")
+plt.plot(historial.history["loss"])
+#plt.show() #with 500 is ok
+result= model.predict([100])
+print (result)
